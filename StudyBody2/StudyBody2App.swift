e59bfc14795ac6baebdfcefb167766bec56a0561
ealmalki23 <147074692+ealmalki23@users.noreply.github.com>
2023-12-03 15:55:22 +0300

    Initial Commit

---
+++ StudyBody2/StudyBody2App.swift
@@ -0,0 +1,17 @@
+//
+//  StudyBody2App.swift
+//  StudyBody2
+//
+//  Created by Eman Almalki  on 19/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct StudyBody2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StudyBody2UITests/StudyBody2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  StudyBody2UITestsLaunchTests.swift
+//  StudyBody2UITests
+//
+//  Created by Eman Almalki  on 19/05/1445 AH.
+//
+
+import XCTest
+
+final class StudyBody2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
